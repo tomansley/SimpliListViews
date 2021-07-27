e115bcd29f5dcb908ae6d3de2345e26e26369fdb
Tom Ansley <tomansley@gmail.com>
2021-07-27 07:42:29 -0500

    Minor changes to a number of different components

--- force-app/main/default/lwc/simpliUIListViewsStart/simpliUIListViewsStart.js
+++ force-app/main/default/lwc/simpliUIListViewsStart/simpliUIListViewsStart.js
@@ -2,30 +2,33 @@ import { LightningElement, wire, track, api  } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import updateAllListViews from '@salesforce/apex/ListViewController.updateAllListViews';
-import getListViewObjects from '@salesforce/apex/ListViewController.getListViewObjects';
+import getIsInitialized from '@salesforce/apex/ListViewController.getIsInitialized';
 
 export default class SimpliUIListViewsStart extends LightningElement {
 
     @track spinner = false;             //identifies if the spinner should be displayed or not.
 
     //for tracking list view init process
-    @track isInit = true;               //indicates whether the list views have been initialized for the first time or not.
+    @track isInitialized = false;               //indicates whether the list views have been initialized for the first time or not.
     @track showProgress = false;        //indicates whether the progress bar should be displayed
     @track batchId = '';                //indicates the batch Id of the list view batch process.
-
-    /*
-     * Wiring to get the list of objects in the system
-     */
-    @wire (getListViewObjects, { includedObjects: '', excludedObjects: ''  })
-    wiredListViewObjects({ error, data }) {
+    
+    @wire (getIsInitialized, { })
+    wiredIsInitialized({ error, data }) {
         if (data) { 
-            
-            if (data === undefined || data.length === 0)
-            {
-                this.isInit = false;
-            }
-
-            this.spinner = false;
+            console.log('Is Initialized called successfully - ' + data); 
+            this.isInitialized = data; 
+            this.error = undefined; 
+        } else if (error) { 
+            this.error = error; 
+            console.log('Error Detected ' + error.message + ' - ' + error.stackTrace); 
+            this.objectActionList = undefined; 
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error Checking Initialization',
+                message: 'There was an error checking for Simpli List Views initialization. Please see an administrator\n\n' + error.message,
+                variant: 'error',
+                mode: 'sticky'
+            }));
         }
     }
     
