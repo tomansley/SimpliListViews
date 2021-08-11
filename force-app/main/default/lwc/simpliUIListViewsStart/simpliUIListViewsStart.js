801c26954371af019206daf79ee1919c83491995
Tom Ansley <tomansley@gmail.com>
2021-08-11 10:45:30 -0500

    Moved all config descriptions to metadata

--- force-app/main/default/lwc/simpliUIListViewsStart/simpliUIListViewsStart.js
+++ force-app/main/default/lwc/simpliUIListViewsStart/simpliUIListViewsStart.js
@@ -15,13 +15,20 @@ import List_View_Processing_Complete from '@salesforce/label/c.List_View_Process
 
 import updateAllListViews from '@salesforce/apex/ListViewController.updateAllListViews';
 import getIsInitialized from '@salesforce/apex/ListViewController.getIsInitialized';
+import getOrgWideDescriptions from '@salesforce/apex/ListViewConfigController.getOrgWideDescriptions';
+import getComponentDescriptions from '@salesforce/apex/ListViewConfigController.getComponentDescriptions';
+import getListViewDescriptions from '@salesforce/apex/ListViewConfigController.getListViewDescriptions';
 
 export default class SimpliUIListViewsStart extends LightningElement {
 
     @track spinner = false;             //identifies if the spinner should be displayed or not.
 
+    @track orgWideDescs;                //holds ORG WIDE description details
+    @track compDescs;                   //holds COMPONENT description details
+    @track listViewDescs;               //holds LIST VIEW description details
+
     //for tracking list view init process
-    @track isInitialized = false;               //indicates whether the list views have been initialized for the first time or not.
+    @track isInitialized = false;       //indicates whether the list views have been initialized for the first time or not.
     @track showProgress = false;        //indicates whether the progress bar should be displayed
     @track batchId = '';                //indicates the batch Id of the list view batch process.
     
@@ -39,6 +46,57 @@ export default class SimpliUIListViewsStart extends LightningElement {
             this.dispatchEvent(new ShowToastEvent({
                 title: 'Error Checking Initialization',
                 message: 'There was an error checking for Simpli List Views initialization. Please see an administrator - ' + error.body.message,
+                variant: 'error',
+                mode: 'sticky'
+            }));
+        }
+    }
+    
+    @wire (getOrgWideDescriptions, { })
+    wiredOrgWideDescriptions({ error, data }) {
+        if (data) { 
+            console.log('Get Org Wide Descriptions called successfully - ' + data); 
+            this.orgWideDescs = data; 
+        } else if (error) { 
+            console.log('Error Detected - ' + error.body.message + ' | ' + error.body.stackTrace);
+            this.objectActionList = undefined; 
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error Retrieving Org Wide Descriptions',
+                message: 'There was an error retrieving the org wide descriptions. Please see an administrator - ' + error.body.message,
+                variant: 'error',
+                mode: 'sticky'
+            }));
+        }
+    }
+    
+    @wire (getComponentDescriptions, { })
+    wiredComponentDescriptions({ error, data }) {
+        if (data) { 
+            console.log('Get Component Descriptions called successfully - ' + data); 
+            this.compDescs = data; 
+        } else if (error) { 
+            console.log('Error Detected - ' + error.body.message + ' | ' + error.body.stackTrace);
+            this.objectActionList = undefined; 
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error Retrieving Component Descriptions',
+                message: 'There was an error retrieving the component descriptions. Please see an administrator - ' + error.body.message,
+                variant: 'error',
+                mode: 'sticky'
+            }));
+        }
+    }
+    
+    @wire (getListViewDescriptions, { })
+    wiredListViewsDescriptions({ error, data }) {
+        if (data) { 
+            console.log('Get List View Descriptions called successfully - ' + data); 
+            this.listViewDescs = data; 
+        } else if (error) { 
+            console.log('Error Detected - ' + error.body.message + ' | ' + error.body.stackTrace);
+            this.objectActionList = undefined; 
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error Retrieving List View Descriptions',
+                message: 'There was an error retrieving the list view descriptions. Please see an administrator - ' + error.body.message,
                 variant: 'error',
                 mode: 'sticky'
             }));
