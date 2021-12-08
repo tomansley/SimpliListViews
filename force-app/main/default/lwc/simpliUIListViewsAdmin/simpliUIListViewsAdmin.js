a5c86492c2c4112c5447ef14503644cd88e6fd58
Tom Ansley <tomansley@gmail.com>
2021-12-08 09:39:47 -0600

    Added admin enhancement for cleaning deleted list views and associated config

--- force-app/main/default/lwc/simpliUIListViewsAdmin/simpliUIListViewsAdmin.js
+++ force-app/main/default/lwc/simpliUIListViewsAdmin/simpliUIListViewsAdmin.js
@@ -15,11 +15,21 @@ import Selected from '@salesforce/label/c.Selected';
 import Save from '@salesforce/label/c.Save';
 import Processing_Status from '@salesforce/label/c.Processing_Status';
 import List_View_Processing_Complete from '@salesforce/label/c.List_View_Processing_Complete';
+import List_Views_Cleaned from '@salesforce/label/c.List_Views_Cleaned';
+import List_Views_Click_For_Cleaning from '@salesforce/label/c.List_Views_Click_For_Cleaning';
+import Clean from '@salesforce/label/c.Clean';
+import Clean_List_Views from '@salesforce/label/c.Clean_List_Views';
+import Cleaning_Status from '@salesforce/label/c.Cleaning_Status';
+import List_View_Cleaning_Complete from '@salesforce/label/c.List_View_Cleaning_Complete';
+import List_Views_Need_Initialized_Verbage from '@salesforce/label/c.List_Views_Need_Initialized_Verbage';
+import List_Views_Initialized_Verbage from '@salesforce/label/c.List_Views_Initialized_Verbage';
+import List_Views_Click_For_Cleaning_Verbage from '@salesforce/label/c.List_Views_Click_For_Cleaning_Verbage';
 
 import getOrgWideConfig from '@salesforce/apex/ListViewAdminController.getOrgWideConfig';
 import saveOrgWideConfig from '@salesforce/apex/ListViewAdminController.saveOrgWideConfig';
 import getObjectNames from '@salesforce/apex/ListViewAdminController.getObjectNames';
 import updateAllListViews from '@salesforce/apex/ListViewController.updateAllListViews';
+import cleanListViews from '@salesforce/apex/ListViewAdminController.cleanListViews';
 import getIsInitialized from '@salesforce/apex/ListViewController.getIsInitialized';
 
 export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningElement) {
@@ -28,8 +38,9 @@ export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningEle
     @track parameters = new Map();       //holds the map of field/value parameter data
     @track spinner = false;             //identifies if the PAGE spinner should be displayed or not.
     @track objNamesList = undefined;
-    @track isInitialized = false;               //indicates whether the list views have been initialized for the first time or not.
+    @track isInitialized = false;       //indicates whether the list views have been initialized for the first time or not.
     @track showProgress = false;        //indicates whether the progress bar should be displayed
+    @track showCleanProgress = false;   //indicates whether the cleaning job progress bar should be displayed
     @track batchId = '';                //indicates the batch Id of the list view batch process.
 
     get booleanList() {
@@ -40,7 +51,9 @@ export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningEle
     }
 
     label = { List_Views_Initialized, List_Views_Need_Initialized, Refresh, Refresh_List_Views, Parameter_Name, Value, Select_A_Value,
-              Available, Selected, Save, Processing_Status, List_View_Processing_Complete }
+              Available, Selected, Save, Processing_Status, List_View_Processing_Complete, List_Views_Cleaned, List_Views_Click_For_Cleaning,
+              Clean, Clean_List_Views, Cleaning_Status, List_View_Cleaning_Complete, List_Views_Need_Initialized_Verbage, List_Views_Initialized_Verbage,
+              List_Views_Click_For_Cleaning_Verbage }
 
     renderedCallback() {
         console.log('SimpliUIListViewsAdmin.renderedCallback started');
@@ -71,7 +84,7 @@ export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningEle
             console.log('Error Detected - ' + error.body.message + ' | ' + error.body.stackTrace + ' for ' + this.pageName);
             this.dispatchEvent(new ShowToastEvent({
                 title: 'Error Checking Initialization',
-                message: 'There was an error checking for Simpli List Views initialization. Please see an administrator - ' + error.body.message,
+                message: 'There was an error checking for Simpli List Views initialization - ' + error.body.message,
                 variant: 'error',
                 mode: 'sticky'
             }));
@@ -95,7 +108,7 @@ export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningEle
             this.spinnerOff(); 
             this.dispatchEvent(new ShowToastEvent({
                 title: 'Error Retrieving Object Names',
-                message: 'There was an error retrieving all object names. Please see an administrator - ' + error.body.message,
+                message: 'There was an error retrieving all object names - ' + error.body.message,
                 variant: 'error',
                 mode: 'sticky'
             }));
@@ -116,7 +129,7 @@ export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningEle
             console.log('Error Detected - ' + error.body.message + ' | ' + error.body.stackTrace);
             this.dispatchEvent(new ShowToastEvent({
                 title: 'Error Handling User Config',
-                message: 'There was an error handling the config. Please see an administrator - ' + error.body.message,
+                message: 'There was an error handling the config - ' + error.body.message,
                 variant: 'error',
                 mode: 'sticky'
             }));
@@ -184,7 +197,7 @@ export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningEle
 
                 this.dispatchEvent(new ShowToastEvent({
                     title: 'Processing Error',
-                    message: 'There was an error saving the admin config. Please see an administrator - ' + error.body.message,
+                    message: 'There was an error saving the admin config - ' + error.body.message,
                     variant: 'error',
                     mode: 'sticky'
                 }));
@@ -209,7 +222,7 @@ export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningEle
             {
                 this.dispatchEvent(new ShowToastEvent({
                     title: 'Processing Error',
-                    message: 'There was an error processing the list views. Please see an administrator - ' + error.body.message,
+                    message: 'There was an error processing the list views - ' + error.body.message,
                     variant: 'error',
                     mode: 'sticky'
                 }));
@@ -236,7 +249,57 @@ export default class SimpliUIListViewsAdmin extends NavigationMixin(LightningEle
             console.log('Error Detected - ' + error.body.message + ' | ' + error.body.stackTrace);
             this.dispatchEvent(new ShowToastEvent({
                 title: 'Processing Error',
-                message: 'There was an error processing the list views. Please see an administrator - ' + error.body.message,
+                message: 'There was an error processing the list views - ' + error.body.message,
+                variant: 'error',
+                mode: 'sticky'
+            }));
+            this.spinnerOff();
+        });
+
+    }
+
+    //called when a user clicks the button to refresh the list views.
+    handleCleanListViewsButtonClick() {
+
+        this.spinnerOn();
+        console.log('Listview cleaning button clicked and updating all list views');
+
+        cleanListViews({ })
+        .then(result => {
+
+            //if we have an error then send an ERROR toast.
+            if (result === 'failed')
+            {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Processing Error',
+                    message: 'There was an error cleaning the list views - ' + error.body.message,
+                    variant: 'error',
+                    mode: 'sticky'
+                }));
+                this.spinnerOff();
+
+            //else send a SUCCESS toast.
+            } else {
+
+                this.batchId = result;
+
+                this.showCleanProgress = true;
+
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'List View Cleaning',
+                    message: 'List view cleaning has started.',
+                    variant: 'success',
+                    mode: 'dismissable'
+                }));
+                this.dispatchEvent(new CustomEvent('cleanlistviewclick'));
+                this.spinnerOff();
+            }
+        })
+        .catch(error => {
+            console.log('Error Detected - ' + error.body.message + ' | ' + error.body.stackTrace);
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Processing Error',
+                message: 'There was an error cleaning the list views - ' + error.body.message,
                 variant: 'error',
                 mode: 'sticky'
             }));
