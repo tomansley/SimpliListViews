a3c0fc93def96ed4297a911d3220b62c349baba0
tomansley <tomansley@gmail.com>
2024-07-24 07:00:51 -0500

    Added stand alone example

--- force-app/main/default/lwc/simpliUIListViewsTestComp/simpliUIListViewsTestComp.js
+++ force-app/main/default/lwc/simpliUIListViewsTestComp/simpliUIListViewsTestComp.js
@@ -1,47 +1,56 @@
 import { LightningElement, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-import getStandAloneColumns from '@salesforce/apex/ListViewTestCompController.getStandAloneColumns';
-import getStandAloneRowData from '@salesforce/apex/ListViewTestCompController.getStandAloneRowData';
+import getData from '@salesforce/apex/ListViewTestCompController.getData';
 
 export default class SimpliUIListViewsTestComp extends LightningElement {
 
     @track selectedAccountId = '';
-    @track standAloneColumnData;
-    @track standAloneRowData;
+    @track standAloneData;
 
-    @wire (getStandAloneColumns, {})
-    wiredGetStandAloneColumns({ error, data }) {
+    @wire (getData, {})
+    wiredGetStandAloneData({ error, data }) {
        if (data) { 
-           this.standAloneColumnData = JSON.stringify(data); 
+           this.standAloneData = data;
        } else if (error) { 
-           this.standAloneColumnData = undefined; 
+           this.standAloneData = undefined; 
            this.dispatchEvent(new ShowToastEvent({
             title: 'Error',
-            message: 'There was an error retrieving the stand alone columns - ' + error.message,
+            message: 'There was an error retrieving the stand alone data - ' + error.message,
             variant: 'error',
             mode: 'sticky'
         })); 
        }
     }
 
-    @wire (getStandAloneRowData, {})
-    wiredgetStandAloneRowData({ error, data }) {
-       if (data) { 
-           this.standAloneRowData = JSON.stringify(data); 
-       } else if (error) { 
-           this.standAloneRowData = undefined; 
-           this.dispatchEvent(new ShowToastEvent({
-            title: 'Error',
-            message: 'There was an error retrieving the stand alone row data - ' + error.message,
-            variant: 'error',
-            mode: 'sticky'
-        })); 
-       }
+    //for event testing
+    handleEventRequest(event) {
+        let eventRequest = { type: 'refreshData'};
+
+        const slvs = this.template.querySelectorAll('c-simpli-u-i-list-views');
+        let slv = null;
+        
+        slvs.forEach(tmpslv => {
+            if (tmpslv.uniqueComponentId.includes("My Button Event Test")) {
+                slv = tmpslv;
+            }
+        });
+
+        slv.eventRequest(eventRequest);
+
+        eventRequest = { type: 'updateSetting', name: "hasMainTitle", value: false};
+        slv.eventRequest(eventRequest);
+
+    }
+
+    //for event testing
+    handleEventResponse(event) {
+        console.log('EVENT RESPONSE - ' + JSON.stringify(event.detail));
     }
 
     handleExample4DataChange(event) {
         console.log('Handling Example 4 Data Change - ' + event.detail.selectedValue);
         this.selectedAccountId = event.detail.selectedValue;
     }
+
 }
