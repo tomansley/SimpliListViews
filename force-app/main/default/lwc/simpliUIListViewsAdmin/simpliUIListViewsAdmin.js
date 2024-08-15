1d4da700b87a7a79b5df34972a6d9e49315a7121
tomansley <tomansley@gmail.com>
2024-08-15 09:03:44 -0500

    Refactoring

--- force-app/main/default/lwc/simpliUIListViewsAdmin/simpliUIListViewsAdmin.js
+++ force-app/main/default/lwc/simpliUIListViewsAdmin/simpliUIListViewsAdmin.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { LightningElement, wire, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
--- force-app/main/default/lwc/simpliUIListViewsFlowFrame/simpliUIListViewsFlowFrame.js
+++ force-app/main/default/lwc/simpliUIListViewsFlowFrame/simpliUIListViewsFlowFrame.js
@@ -1,4 +1,5 @@
 import {LightningElement, api, wire, track} from 'lwc';
+import * as SLVHelper from 'c/simpliUIListViewsHelper';
 
 import getListViewAction from '@salesforce/apex/ListViewController.getListViewAction';
 import Close from '@salesforce/label/c.Close';
@@ -6,10 +7,34 @@ import Close from '@salesforce/label/c.Close';
 export default class SimpliUIListViewsFlowFrame extends LightningElement {
 
     @api showModal;                 //indicates whether this modal dialog should be displayed or not.
-    @api recordIds;                 //a concatenated string of record Ids
     @api actionApiName;             //the flow that was selected.
     @track listViewAction;          //holds the action data for the provided action API name.
-    @track isInitialized = false;;  //identifies if the frame has been initialized
+    @track isInitialized = false;  //identifies if the frame has been initialized
+
+    _recordIds;
+    @api set recordIds(value) 
+    { 
+        //the record ids come through as a set. So we need to change it to an array
+        this._recordIds = [];
+        if (!SLVHelper.isEmpty(value)) {        
+            value.forEach(recordId => {
+                this._recordIds.push(recordId);
+            });
+        }
+
+        //the LWC object that is required for the lightning-flow component
+        this._recordIds = [{
+            name: 'recordIds',
+            type: 'String',
+            value: this._recordIds
+        }];
+    }
+    
+    get recordIds() 
+    { 
+        return this._recordIds; 
+    }
+
 
     label = { Close };
 
@@ -21,26 +46,28 @@ export default class SimpliUIListViewsFlowFrame extends LightningElement {
             .then(result => {
                 console.log('getListViewAction successfully called'); 
                 this.listViewAction = result;
-                this.hasParameters = this.listViewAction.hasDisplayParameters;
-                this.spinner = false;
             })
             .catch(error => {
                 console.log('Error Detected - ' + error.message + ' | ' + error.stackTrace);
-                this.spinner = false;
                 return;
             });
         }
+
     }
 
-    finishFlow() {
-        this.dispatchEvent(new CustomEvent('finished'));
+    handleFlowStatusChange(event) {
+        if (event.detail.status === 'FINISHED') {
+            this.dispatchEvent(new CustomEvent('finished'));
+        }
     }
 
     handleCancelClick() {
         this.dispatchEvent(new CustomEvent('cancelled'));
+        this.isInitialized = false;
     }
 
     handleClose() {
         this.dispatchEvent(new CustomEvent('cancelled'));
+        this.isInitialized = false;
     }
 }
